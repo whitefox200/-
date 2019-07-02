d863fd3664889abcf1117e86a59710d2ac9eeb7e
whitefox200 <42363694+whitefox200@users.noreply.github.com>
2019-07-02 15:09:02 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "$";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+
+
+
+
+
+ 
+client.login("NTk1NTU5ODg1OTc5MjU0Nzg0.XRswjg.BNH6jTsEvDiycwA4sKIXNHH7G28");
